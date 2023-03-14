07ad60e2feee4287ca9a4874d4dafe97e6c93da1
yamoto0628 <a18sb009@zr.osaka-cu.ac.jp>
2023-03-14 15:43:00 +0900

    implement

--- analyzer/cmd/graph.go
+++ analyzer/cmd/graph.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -26,17 +27,23 @@ type SourceVulRecord struct {
 }
 
 type VulRecord struct {
-	PackageId           string `json:"package_id"`
-	IntroducedVersionId string `json:"introduced_version_id"`
-	FixedVersionId      string `json:"fixed_version_id"`
-	DependencyId        int64  `json:"dependency_id"`
+	PackageId           string        `json:"package_id"`
+	IntroducedVersionId int64         `json:"introduced_version_id"`
+	IntroducedTimestamp time.Time     `json:"introduced_timestamp"`
+	FixedVersionId      int64         `json:"fixed_version_id"`
+	FixedTimestamp      time.Time     `json:"fixed_timestamp"`
+	RawVulDuration      time.Duration `json:"raw_vul_duration"`
+	VulDuration         time.Duration `json:"vul_duration"`
+	DependencyId        int64         `json:"dependency_id"`
 }
 
 type Neo4jRecord struct {
-	AffectedPackageId            string `json:"affected_package_id"`
-	AffectedPackageVersionId     string `json:"affected_version_id"`
-	AffectedPackageNextVersionId string `json:"affected_version_next_id"`
-	DependencyId                 int64  `json:"dependency_id"`
+	AffectedPackageId                          string    `json:"affected_package_id"`
+	AffectedPackageVersionId                   int64     `json:"affected_version_id"`
+	AffectedPackagePublishTimestamp            time.Time `json:"affected_package_publish_timestamp"`
+	AffectedPackageNextVersionId               int64     `json:"affected_version_next_id"`
+	AffectedPackageNextVersionPublishTimestamp time.Time `json:"affected_package_next_version_publish_timestamp"`
+	DependencyId                               int64     `json:"dependency_id"`
 }
 
 func AnalyzeWithGraphDB(vulPackageInputFile string) error {
@@ -72,9 +79,15 @@ func AnalyzeWithGraphDB(vulPackageInputFile string) error {
 				vulPackage.Summary,
 				vulPackage.PackageName,
 				affectedPackageVersion.PackageId,
-				affectedPackageVersion.IntroducedVersionId,
-				affectedPackageVersion.FixedVersionId,
+				strconv.FormatInt(affectedPackageVersion.IntroducedVersionId, 10),
+				strconv.FormatInt(affectedPackageVersion.FixedVersionId, 10),
 				strconv.FormatInt(affectedPackageVersion.DependencyId, 10),
+				// 単位はday
+				strconv.FormatFloat(affectedPackageVersion.VulDuration.Seconds()/3600/24, 'f', -1, 64),
+				strconv.FormatFloat(affectedPackageVersion.RawVulDuration.Seconds()/3600/24, 'f', -1, 64),
+				//
+				affectedPackageVersion.IntroducedTimestamp.String(),
+				affectedPackageVersion.FixedTimestamp.String(),
 			}); err != nil {
 				return err
 			}
@@ -84,6 +97,14 @@ func AnalyzeWithGraphDB(vulPackageInputFile string) error {
 	}
 
 	return nil
+}
+
+func ParseIntMust(s string) int64 {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
 }
 
 func findAffectedPackageVersions(driver neo4j.DriverWithContext, VulPackageVersionIdsString string, deps int64) ([]VulRecord, error) {
@@ -100,8 +121,10 @@ func findAffectedPackageVersions(driver neo4j.DriverWithContext, VulPackageVersi
 		queryString := fmt.Sprintf(`
 MATCH (affected_version:verison)-[dependency*%d..%d]->(affecting_version:verison), (affected_version)-[n:next]->(affected_version_next:verison)
 WHERE affecting_version.version_id IN [%s]
-RETURN DISTINCT affected_version.package_id AS affected_package_id, affected_version.version_id AS affected_version_id, affected_version_next.version_id AS affected_version_next_id, id(dependency[%d]) AS dependency_id
-ORDER BY affected_package_id ASC, dependency_id ASC, affected_version_id ASC;
+RETURN DISTINCT affected_version.package_id AS affected_package_id, affected_version.version_id AS affected_version_id, 
+affected_version_next.version_id AS affected_version_next_id, id(dependency[%d]) AS dependency_id,
+affected_version.published_timestamp AS affected_version_published_timestamp, affected_version_next.published_timestamp AS affected_version_next_published_timestamp
+ORDER BY affected_package_id ASC, dependency_id ASC, affected_version_published_timestamp ASC;
 		`, deps, deps, strings.Join(vulPackageVersionIds, ","), deps-1)
 		fmt.Printf("query: =====\n\n %s \n\n ====\n", queryString)
 
@@ -114,10 +137,12 @@ ORDER BY affected_package_id ASC, dependency_id ASC, affected_version_id ASC;
 		for result.Next(ctx) {
 			r := result.Record()
 			records = append(records, Neo4jRecord{
-				AffectedPackageId:            r.Values[0].(string),
-				AffectedPackageVersionId:     r.Values[1].(string),
-				AffectedPackageNextVersionId: r.Values[2].(string),
-				DependencyId:                 r.Values[3].(int64),
+				AffectedPackageId:                          r.Values[0].(string),
+				AffectedPackageVersionId:                   ParseIntMust(r.Values[1].(string)),
+				AffectedPackageNextVersionId:               ParseIntMust(r.Values[2].(string)),
+				DependencyId:                               r.Values[3].(int64),
+				AffectedPackagePublishTimestamp:            r.Values[4].(time.Time),
+				AffectedPackageNextVersionPublishTimestamp: r.Values[5].(time.Time),
 			})
 		}
 
@@ -129,51 +154,173 @@ ORDER BY affected_package_id ASC, dependency_id ASC, affected_version_id ASC;
 
 	records := rs.([]Neo4jRecord)
 	vulRecords := make([]VulRecord, 0)
-	nowDependencyId := int64(0)
+	nowPackageId := ""
+	samePackageVulRecords := make([]VulRecord, 0)
 	for len(records) != 0 {
-		introducedVersionId := records[0].AffectedPackageVersionId
-		nowDependencyId = records[0].DependencyId
+		if records[0].AffectedPackageId != nowPackageId {
+			// TODO: リフレッシュ
+			if _, err := refreshVulRecords(samePackageVulRecords); err != nil {
+				return nil, err
+			}
+			vulRecords = append(vulRecords, samePackageVulRecords...)
 
-		fixedVersionId, deletingIndexes := findFixedVersion(records)
-		vulRecords = append(vulRecords, VulRecord{
+			nowPackageId = records[0].AffectedPackageId
+			samePackageVulRecords = make([]VulRecord, 0)
+		}
+
+		// 今のパッケージの中で、同じ依存関係のものをまとめて，それをもとにfixedVersionを探す
+		fixedVersionId, vulDuration, deletingIndexes, err := findFixedVersion(records)
+		if err != nil {
+			return nil, err
+		}
+		vukRecord := VulRecord{
 			PackageId:           records[0].AffectedPackageId,
-			IntroducedVersionId: introducedVersionId,
+			IntroducedVersionId: records[0].AffectedPackageVersionId,
+			IntroducedTimestamp: records[0].AffectedPackagePublishTimestamp,
 			FixedVersionId:      fixedVersionId,
-			DependencyId:        nowDependencyId,
-		})
+			FixedTimestamp:      records[0].AffectedPackageNextVersionPublishTimestamp,
+			RawVulDuration:      vulDuration,
+			VulDuration:         vulDuration,
+			DependencyId:        records[0].DependencyId,
+		}
+
+		samePackageVulRecords = append(samePackageVulRecords, vukRecord)
+		records = records[1:]
 
 		// すでに調べた要素は削除(indexずれるので後ろから消す)
 		for i := len(deletingIndexes) - 1; i >= 0; i-- {
 			records = append(records[:deletingIndexes[i]], records[deletingIndexes[i]+1:]...)
 		}
-		records = records[1:]
 	}
 
 	log.Printf("脆弱性レコード数: %d", len(vulRecords))
 	return vulRecords, nil
 }
 
-func findFixedVersion(records []Neo4jRecord) (string, []int) {
+// 同project_idのものしか入らない
+// 前提: IntroducedTimestampが新しい順にソートされている
+// memo: https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d3a957bf-bf55-412f-a817-f24f0c344f46/%E7%A0%94%E7%A9%B6%E3%83%A1%E3%83%A2-9.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230314%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230314T021234Z&X-Amz-Expires=86400&X-Amz-Signature=22bc4297680f52aba51b61c11325c1c3da7b8771dbba43591e7a84970463db26&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22%25E7%25A0%2594%25E7%25A9%25B6%25E3%2583%25A1%25E3%2583%25A2-9.jpg%22&x-id=GetObject
+type durations []duration
+
+type duration struct {
+	start time.Time
+	end   time.Time
+}
+
+func refreshVulRecords(samePackageVulRecords []VulRecord) ([]VulRecord, error) {
+	if len(samePackageVulRecords) == 0 {
+		return []VulRecord{}, nil
+	}
+
+	beforeTime := samePackageVulRecords[0].IntroducedTimestamp
+	for i, _ := range samePackageVulRecords {
+		if i == 0 {
+			continue
+		}
+
+		if beforeTime.Equal(samePackageVulRecords[i].IntroducedTimestamp) ||
+			beforeTime.After(samePackageVulRecords[i].IntroducedTimestamp) {
+			continue
+		}
+		dul := duration{
+			start: beforeTime,
+			end:   samePackageVulRecords[i].IntroducedTimestamp,
+		}
+		beforeTime = samePackageVulRecords[i].IntroducedTimestamp
+
+		if dul.start.Equal(dul.end) {
+			continue
+		}
+
+		// 範囲の中で被ってるやつ洗い出す
+		overlappedIndexes := make([]int, 0)
+		for j := 0; j < len(samePackageVulRecords); j++ {
+			if i == j {
+				continue
+			}
+
+			if (samePackageVulRecords[j].IntroducedTimestamp.After(dul.start) ||
+				samePackageVulRecords[j].IntroducedTimestamp.Equal(dul.start)) &&
+				(samePackageVulRecords[j].IntroducedTimestamp.Before(dul.end) ||
+					samePackageVulRecords[j].IntroducedTimestamp.Equal(dul.end)) {
+				overlappedIndexes = append(overlappedIndexes, j)
+			}
+
+			if samePackageVulRecords[j].IntroducedTimestamp.After(dul.end) {
+				break
+			}
+		}
+
+		//log.Printf("duration: %v~%v", dul.start, dul.end)
+		// 被ってるやつで割り算
+		for _, j := range overlappedIndexes {
+			//log.Printf("dulation: %v, 分母: %v, start_id: %v, end_id: %v",
+			//	dul.end.Sub(dul.start).Seconds()/3600/24,
+			//	len(overlappedIndexes),
+			//	samePackageVulRecords[j].IntroducedVersionId,
+			//	samePackageVulRecords[j].FixedVersionId,
+			//)
+
+			start := time.Time{}
+			if samePackageVulRecords[j].IntroducedTimestamp.After(dul.start) {
+				start = samePackageVulRecords[j].IntroducedTimestamp
+			} else {
+				start = dul.start
+			}
+
+			end := time.Time{}
+			if samePackageVulRecords[j].FixedTimestamp.Before(dul.end) {
+				end = samePackageVulRecords[j].FixedTimestamp
+			} else {
+				end = dul.end
+			}
+
+			samePackageVulRecords[j].VulDuration -= end.Sub(start) /
+				(time.Duration(len(overlappedIndexes))) * time.Duration(len(overlappedIndexes)-1)
+		}
+		//log.Print("\n\n")
+	}
+
+	return samePackageVulRecords, nil
+}
+
+// 2の方が大きいときは正の値
+func getTimestampDiff(timestamp1, timestamp2 time.Time) (time.Duration, error) {
+	return timestamp2.Sub(timestamp1), nil
+}
+
+func findFixedVersion(records []Neo4jRecord) (int64, time.Duration, []int, error) {
 	deletingIndexes := make([]int, 0)
 	introducedRecord := records[0]
 
 	dependencyId := introducedRecord.DependencyId
 	nextVersionId := introducedRecord.AffectedPackageNextVersionId
+	nextPublishTimestamp := introducedRecord.AffectedPackageNextVersionPublishTimestamp
 
 	for i, record := range records {
 		// 違うdependencyIdに差し掛かったらbreak
 		if record.DependencyId != dependencyId {
-			return records[i-1].AffectedPackageNextVersionId, deletingIndexes
+			duration, err := getTimestampDiff(introducedRecord.AffectedPackagePublishTimestamp, records[i-1].AffectedPackageNextVersionPublishTimestamp)
+
+			if err != nil {
+				return 0, 0, nil, err
+			}
+			return records[i-1].AffectedPackageNextVersionId, duration, deletingIndexes, nil
 		}
 
 		// 同じ依存関係内で途切れるまで探索する
 		if nextVersionId == record.AffectedPackageVersionId {
 			nextVersionId = record.AffectedPackageNextVersionId
+			nextPublishTimestamp = record.AffectedPackageNextVersionPublishTimestamp
 			deletingIndexes = append(deletingIndexes, i)
 		}
 	}
 
-	return nextVersionId, deletingIndexes
+	duration, err := getTimestampDiff(introducedRecord.AffectedPackagePublishTimestamp, nextPublishTimestamp)
+	if err != nil {
+		return 0, 0, nil, err
+	}
+	return nextVersionId, duration, deletingIndexes, nil
 }
 
 func buildSourceVulRecords(rows [][]string) []SourceVulRecord {
@@ -200,6 +347,10 @@ func writeHeader(writer *csv.Writer) error {
 		"vul_start_version",
 		"vul_end_version",
 		"dependency_id",
+		"vul_duration",
+		"raw_vul_duration",
+		"vul_start_version_published_at",
+		"vul_end_version_published_at",
 	})
 }
 
